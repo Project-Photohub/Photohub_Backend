35c44861732f1978cdd2cbcde04e0d2b6662cb2e
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-04 22:39:28 +0900

    FEAT :: Entities

---
+++ src/main/kotlin/com/example/photohub/data/TableNames.kt
@@ -0,0 +1,10 @@
+package com.example.photohub.data
+
+object TableNames {
+
+    const val USER = "tbl_user"
+
+    const val GROUP = "tbl_group"
+    const val MEMBER = "tbl_member"
+    const val PHOTO_CARD = "tbl_photo_card"
+}
