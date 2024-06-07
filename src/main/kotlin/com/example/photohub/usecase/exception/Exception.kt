72b9790f155be31ea8235218f2cdb508e9cdc176
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-07 22:09:37 +0900

    FEAT :: Exception.kt

---
+++ src/main/kotlin/com/example/photohub/usecase/exception/Exception.kt
@@ -0,0 +1,36 @@
+package com.example.photohub.usecase.exception
+
+open class StatusCodeException protected constructor(
+    val statusCode: Int,
+    override val message: String
+) : RuntimeException()
+
+class BusinessException(
+    statusCode: Int,
+    message: String
+) : StatusCodeException(statusCode, message) {
+    companion object {
+        val ENTITY_ID_NOT_ASSIGNED_BUT_CALL
+            get() = BusinessException(500, "Entity's Id is not assigned BUT Called.")
+    }
+}
+
+class AuthException(
+    statusCode: Int,
+    message: String
+) : StatusCodeException(statusCode, message) {
+    companion object {
+        val INVALID_TOKEN
+            get() = AuthException(401, "Invalid token.")
+    }
+}
+
+class SecurityException(
+    statusCode: Int,
+    message: String
+) : StatusCodeException(statusCode, message) {
+    companion object {
+        val ACCESS_DENIED_TO_RESOURCE
+            get() = SecurityException(403, "Access Denied to Resource.")
+    }
+}
