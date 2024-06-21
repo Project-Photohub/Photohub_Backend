50a39f37cd931f9ebfa6592f0edacb029f18eeda
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-21 14:05:33 +0900

    FEAT :: Editor

---
+++ src/main/kotlin/com/example/photohub/infra/editor/Editor.kt
@@ -0,0 +1,8 @@
+package com.example.photohub.infra.editor
+
+import org.springframework.stereotype.Component
+
+@Component
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+annotation class Editor()
