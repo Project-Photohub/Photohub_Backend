18cdc27cd8d7bde45cac0f25f064fb59eb4abf02
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-21 14:06:21 +0900

    FEAT :: FilterChain 적용

---
+++ src/main/kotlin/com/example/photohub/infra/config/filter/ApplicationFilterChainConfig.kt
@@ -0,0 +1,35 @@
+package com.example.photohub.infra.config.filter
+
+import com.example.photohub.infra.editor.request.ResponseEditorFactory
+import com.example.photohub.infra.exception.ErrorLogResponseFilter
+import com.example.photohub.infra.exception.ExceptionConvertFilter
+import com.example.photohub.infra.exception.ExceptionResolverFilterChain
+import com.example.photohub.infra.exception.ExceptionResolverFilterChainProxy
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ApplicationFilterChainConfig(
+    private val objectMapper: ObjectMapper,
+    private val responseEditorFactory: ResponseEditorFactory
+) {
+
+    @Bean
+    fun exceptionResolverFilterChain(): FilterRegistrationBean<ExceptionResolverFilterChainProxy> {
+        val filterChainProxy = ExceptionResolverFilterChainProxy(
+            ExceptionResolverFilterChain(
+                listOf(
+                    ExceptionConvertFilter(objectMapper),
+                    ErrorLogResponseFilter(responseEditorFactory)
+                )
+            )
+        )
+
+        val registration = FilterRegistrationBean<ExceptionResolverFilterChainProxy>(filterChainProxy)
+
+        registration.order = -2_100_000_000
+        return registration
+    }
+}
