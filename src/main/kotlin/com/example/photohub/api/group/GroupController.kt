df81b058185167541a28e29fc5feef6c1741f4dd
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-26 09:51:28 +0900

    FEAT :: Controller

--- src/main/kotlin/com/example/photohub/api/group/GroupController.kt
+++ src/main/kotlin/com/example/photohub/api/group/GroupController.kt
@@ -3,26 +3,51 @@ package com.example.photohub.api.group
 import com.example.photohub.Controller
 import com.example.photohub.security.authentication.annotation.RequireRole
 import com.example.photohub.security.authentication.vo.Role
+import com.example.photohub.usecase.group.dto.request.CreateGroupRequest
 import com.example.photohub.usecase.group.dto.response.GroupInfoListDto
 import com.example.photohub.usecase.group.dto.response.GroupMemberInfoListDto
+import com.example.photohub.usecase.group.port.`in`.CreateGroupUseCase
 import com.example.photohub.usecase.group.port.`in`.GetAllGroupInfoUseCase
 import com.example.photohub.usecase.group.port.`in`.GetAllGroupMemberInfoUseCase
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import jakarta.validation.Valid
+import org.jetbrains.annotations.NotNull
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 
 @RequestMapping("/groups")
 @Controller
 class GroupController(
     private val getAllGroupInfoUseCase: GetAllGroupInfoUseCase,
-    private val getAllGroupMemberInfoUseCase: GetAllGroupMemberInfoUseCase
+    private val getAllGroupMemberInfoUseCase: GetAllGroupMemberInfoUseCase,
+    private val createGroupUseCase: CreateGroupUseCase
 ) {
 
     @GetMapping("/all")
     fun getAllGroupInfo(): GroupInfoListDto =
         getAllGroupInfoUseCase()
 
-    @RequireRole(Role.ADMIN)
     @GetMapping("/all/member")
     fun getAllGroupMemberInfo(): GroupMemberInfoListDto =
         getAllGroupMemberInfoUseCase()
+
+    @RequireRole(Role.MANAGER)
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    fun create(
+        @Valid
+        @NotNull
+        @RequestParam
+        name: String,
+        @Valid
+        @NotNull
+        logo: MultipartFile
+    ) {
+        createGroupUseCase(
+            CreateGroupRequest(
+                name = name,
+                logo = logo
+            )
+        )
+    }
 }
--- src/main/kotlin/com/example/photohub/usecase/group/dto/request/CreateMemberRequest.kt
+++ src/main/kotlin/com/example/photohub/usecase/group/dto/request/CreateMemberRequest.kt
@@ -1,7 +1,19 @@
 package com.example.photohub.usecase.group.dto.request
 
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Positive
+import org.jetbrains.annotations.NotNull
+
 data class CreateMemberRequest(
+    @Valid
+    @NotNull
     val name: String,
-    val nickname: String,
+
+    @Valid
+    val nickname: String?,
+
+    @Valid
+    @Positive
+    @NotNull
     val groupId: Long
 )
